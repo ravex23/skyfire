626345a53aef1b2c6efa91c993274913e62a7552
ravex23 <reminiscing_23@hotmail.com>
2026-02-04 18:07:43 +0500

    chore: ignore generated IAM policy and clean up repo

--- infra/eks/main.tf
+++ infra/eks/main.tf
@@ -35,7 +35,7 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
